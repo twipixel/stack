08a5c422fd51e32a7b576ed56accbb4717fa9d0d
twipixel <twipixel@naver.com>
2017-06-01 18:49:45 +0900

    P2 물리 환경 셋팅 중

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,5 +1,7 @@
 export default {
-    GAME_WIDTH: 720,
-    GAME_HEIGHT: 1142,
+    //GAME_WIDTH: 720,
+    //GAME_HEIGHT: 1142,
+    GAME_WIDTH: 540,
+    GAME_HEIGHT: 540,
     PREFIX_ASSET_URL: './../assets/',
 }
